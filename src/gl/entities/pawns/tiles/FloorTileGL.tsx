dde279184f2db0cc80e2a15e9781b220a446a88e
Josh Burgess <joshuacburgess32@gmail.com>
2022-10-16 16:57:11 +0100

    Add wall slots to floor

--- src/gl/entities/pawns/tiles/FloorTileGL.tsx
+++ src/gl/entities/pawns/tiles/FloorTileGL.tsx
@@ -1,7 +1,13 @@
-import { group } from "console";
 import React, { FunctionComponent, ReactNode } from "react";
 
 const FLOOR_SIZE = 6;
+
+export const FLOOR_WALL_SLOTS = {
+  1: { pos: [2.75, 0, 0], rot: 0 },
+  2: { pos: [-2.75, 0, 0], rot: 0 },
+  3: { pos: [0, 0, 2.75], rot: Math.PI / 2 },
+  4: { pos: [0, 0, -2.75], rot: Math.PI / 2 },
+};
 
 interface IFloorTileGLProps {
   children?: ReactNode;
