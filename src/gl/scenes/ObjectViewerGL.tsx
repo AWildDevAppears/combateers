ddccc871f844b9304915b1fc38e796e6731451a3
Josh Burgess <joshuacburgess32@gmail.com>
2022-10-23 13:17:51 +0100

    Add seperate object viewer

---
+++ src/gl/scenes/ObjectViewerGL.tsx
@@ -0,0 +1,19 @@
+import { OrbitControls } from "@react-three/drei";
+import React, { FunctionComponent, ReactNode } from "react";
+
+interface IObjectViewerGLProps {
+  children?: ReactNode;
+}
+
+export const ObjectViewerGL: FunctionComponent<IObjectViewerGLProps> = ({
+  children,
+}) => {
+  return (
+    <group>
+      <OrbitControls />
+      <ambientLight />
+
+      {children}
+    </group>
+  );
+};
