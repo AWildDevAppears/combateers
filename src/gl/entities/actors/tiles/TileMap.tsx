d9c0152de37a526ce6800db3a54eec5d435a9b92
Josh Burgess <joshuacburgess32@gmail.com>
2022-10-29 15:31:50 +0100

    Add spawn positions

--- src/gl/entities/actors/tiles/TileMap.tsx
+++ src/gl/entities/actors/tiles/TileMap.tsx
@@ -3,6 +3,7 @@ import { useFrame } from "@react-three/fiber";
 import React, {
   FunctionComponent,
   ReactNode,
+  useCallback,
   useEffect,
   useMemo,
   useRef,
@@ -19,74 +20,98 @@ import { GroundGL } from "./GroundGL";
 
 interface ITileMapProps {
   tileset: Array<Array<string>>;
+  onGetPlayerSpawn: (spawnPos: [number, number, number]) => void;
+  children: ReactNode;
 }
 
 const TILE_OFFSET = 6;
-
-const createStructs = (structWithPos: string, idx: number) => {
-  const [struct, pos, rot] = structWithPos.split("-");
-
-  const position = Reflect.has(StructurePositions, pos)
-    ? StructurePositions[pos]
-    : StructurePositions.P1;
-
-  // Allow objects to be rotated to fixed angles
-  const rotation = Reflect.has(StructureRotations, rot)
-    ? StructureRotations[rot]
-    : StructureRotations.R1;
-
-  if (Reflect.has(Structures, struct)) {
-    const Component = Structures[struct];
-    return <Component key={idx} position={position} rotateY={rotation} />;
-  }
-};
-
-const mapTiles = (tileHash: string, idx: number): ReactNode => {
-  const [tileID, rPos, structures] = tileHash.toUpperCase().split(":");
-
-  const injectableStructures = structures?.split(",").map(createStructs);
-
-  const position = Reflect.has(RotationPositions, rPos)
-    ? RotationPositions[rPos]
-    : RotationPositions.P1;
-
-  if (Reflect.has(Tiles, tileID)) {
-    const Component = Tiles[tileID];
-    return (
-      <Component
-        key={idx}
-        position={[TILE_OFFSET * idx, 0, 0]}
-        rotateY={position}
-      >
-        {injectableStructures}
-      </Component>
-    );
-  }
-
-  return (
-    <FloorTileGL position={[TILE_OFFSET * idx, 0, 0]} rotateY={position} />
-  );
-};
 
 /**
  * Take an array of tile identifiers, create their tiles and plot them out on a grid.
  * Once we have our tileset, apply a single plane to the bottom of it to act as the ground.
  */
-export const TileMap: FunctionComponent<ITileMapProps> = ({ tileset }) => {
+export const TileMap: FunctionComponent<ITileMapProps> = ({
+  tileset,
+  onGetPlayerSpawn,
+  children,
+}) => {
   const maxTiles = useRef(0);
+  const [tiles, setTiles] = useState<Array<ReactNode>>([]);
+  const [floorSize, setFloorSize] = useState<[number, number]>();
+  const [floorPos, setFloorPos] = useState<[number, number, number]>();
 
-  const mapRow = (tileRow: Array<string>, idx: number) => {
+  const currentRow = useRef<number>(0);
+  const currentCol = useRef<number>(0);
+
+  const createStructs = useCallback((structWithPos: string, idx: number) => {
+    const [struct, pos, rot] = structWithPos.split("-");
+
+    if (struct === "E") {
+      // We found the players spawn, return it so we can drop the player into the right place.
+      onGetPlayerSpawn([
+        currentRow.current * TILE_OFFSET - TILE_OFFSET,
+        0,
+        currentCol.current * TILE_OFFSET - TILE_OFFSET,
+      ]);
+    }
+
+    const position = Reflect.has(StructurePositions, pos)
+      ? StructurePositions[pos]
+      : StructurePositions.P1;
+
+    // Allow objects to be rotated to fixed angles
+    const rotation = Reflect.has(StructureRotations, rot)
+      ? StructureRotations[rot]
+      : StructureRotations.R1;
+
+    if (Reflect.has(Structures, struct)) {
+      const Component = Structures[struct];
+      return <Component key={idx} position={position} rotateY={rotation} />;
+    }
+  }, []);
+
+  const mapTiles = useCallback((tileHash: string, idx: number): ReactNode => {
+    const [tileID, rPos, structures] = tileHash.toUpperCase().split(":");
+    currentCol.current = idx + 1;
+
+    const injectableStructures = structures?.split(",").map(createStructs);
+
+    const position = Reflect.has(RotationPositions, rPos)
+      ? RotationPositions[rPos]
+      : RotationPositions.P1;
+
+    if (Reflect.has(Tiles, tileID)) {
+      const Component = Tiles[tileID];
+      return (
+        <Component
+          key={idx}
+          position={[TILE_OFFSET * idx, 0, 0]}
+          rotateY={position}
+        >
+          {injectableStructures}
+        </Component>
+      );
+    }
+
+    return (
+      <FloorTileGL
+        key={idx}
+        position={[TILE_OFFSET * idx, 0, 0]}
+        rotateY={position}
+      />
+    );
+  }, []);
+
+  const mapRow = useCallback((tileRow: Array<string>, idx: number) => {
     maxTiles.current = Math.max(tileRow.length, maxTiles.current);
+
+    currentRow.current = idx + 1;
     return (
       <group key={idx} position={[0, 0, TILE_OFFSET * idx]}>
         {tileRow.map(mapTiles)}
       </group>
     );
-  };
-
-  const [tiles, setTiles] = useState<Array<ReactNode>>([]);
-  const [floorSize, setFloorSize] = useState<[number, number]>();
-  const [floorPos, setFloorPos] = useState<[number, number, number]>();
+  }, []);
 
   useEffect(() => {
     maxTiles.current = 0;
@@ -95,6 +120,8 @@ export const TileMap: FunctionComponent<ITileMapProps> = ({ tileset }) => {
     const floorHeight = maxTiles.current * TILE_OFFSET;
 
     setFloorSize([floorHeight, floorWidth]);
+
+    console.log("Floor", floorHeight, floorWidth);
 
     // Position should be the exact middle of all of the tiles we are creating
     setFloorPos([
@@ -110,6 +137,7 @@ export const TileMap: FunctionComponent<ITileMapProps> = ({ tileset }) => {
       {floorPos && floorSize && (
         <GroundGL position={floorPos} size={floorSize} />
       )}
+      {children}
     </group>
   );
 };
--- src/gl/scenes/QuestSceneGL/QuestSceneGL.tsx
+++ src/gl/scenes/QuestSceneGL/QuestSceneGL.tsx
@@ -1,7 +1,7 @@
 import { Physics } from "@react-three/cannon";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { Tilesets } from "../../../data/Tilesets";
 import { TileMap } from "../../entities/actors/tiles/TileMap";
 import { PawnGL } from "../../entities/pawn/PawnGL";
@@ -10,14 +10,20 @@ import { CollisionProviderGL } from "../../logistics/collisions/CollisionProvide
 interface IQuestSceneGLProps {}
 
 export const QuestSceneGL: FunctionComponent<IQuestSceneGLProps> = () => {
+  const [playerPos, setPlayerPosition] = useState<[number, number, number]>();
+
   return (
     <Canvas>
       <OrbitControls />
       <ambientLight />
       <CollisionProviderGL>
         <Physics>
-          <TileMap tileset={Tilesets.collCheck}></TileMap>
-          <PawnGL position={[1, 1, 1]} lockIdle={false} />
+          <TileMap
+            tileset={Tilesets.devRoom}
+            onGetPlayerSpawn={setPlayerPosition}
+          >
+            {playerPos && <PawnGL position={playerPos} lockIdle={false} />}
+          </TileMap>
         </Physics>
       </CollisionProviderGL>
     </Canvas>
