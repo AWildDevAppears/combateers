3db5ddeb87a24128e21be3896b3addc68bdd922a
Josh Burgess <joshuacburgess32@gmail.com>
2022-11-12 16:32:19 +0000

    uImproved container inventory

--- src/common/gameUI/ContainerInventory/ContainerInventory.tsx
+++ src/common/gameUI/ContainerInventory/ContainerInventory.tsx
@@ -1,24 +1,37 @@
-import React, { FunctionComponent, useContext } from "react";
+import React, { FunctionComponent, useContext, useEffect } from "react";
+import { IItem } from "../../../data/Items";
 import { QuestContext } from "../../../gl/logistics/quest/QuestProviderGL/QuestProviderGL";
 import { Container } from "../../components/Container/Container";
 import { Inventory } from "../../components/Inventory/Inventory";
+import { InputContext } from "../../services/input/InputProvider";
 
 import style from "./ContainerInventory.module.css";
 
 interface IContainerInventoryProps {
   isOpen: boolean;
+  items?: IItem[];
 }
 
 export const ContainerInventory: FunctionComponent<
   IContainerInventoryProps
-> = ({ isOpen }) => {
+> = ({ isOpen, items = [] }) => {
   const { endInteraction } = useContext(QuestContext);
+  const { negative } = useContext(InputContext).inputs;
+
+  useEffect(() => {
+    if (!isOpen) return;
+
+    if (negative) {
+      endInteraction();
+    }
+  }, [isOpen, negative]);
+
   return (
     <div>
       {isOpen && (
         <div className={style.base}>
           <Container>
-            <Inventory size={12} items={[]} />
+            <Inventory size={Math.max(12, items.length)} items={items} />
 
             <button onClick={endInteraction}>Close</button>
           </Container>
