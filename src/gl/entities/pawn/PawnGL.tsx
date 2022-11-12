5d38e66eef6f75f1aa80e1134a2a8a15372f4635
Josh Burgess <joshuacburgess32@gmail.com>
2022-11-12 16:34:32 +0000

    Add mesh for Player

--- src/gl/entities/pawn/PawnGL.tsx
+++ src/gl/entities/pawn/PawnGL.tsx
@@ -1,7 +1,22 @@
-import { useFrame } from "@react-three/fiber";
-import { MeshCollider, RigidBody, RigidBodyApi } from "@react-three/rapier";
-import React, { FunctionComponent, useContext, useEffect, useRef } from "react";
-import { Vector3 } from "three";
+import { useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { CapsuleCollider, RigidBody, RigidBodyApi } from "@react-three/rapier";
+import React, {
+  FunctionComponent,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+} from "react";
+import {
+  Camera,
+  Group,
+  Matrix4,
+  Mesh,
+  Object3D,
+  Quaternion,
+  Vector3,
+} from "three";
 import { InputContext } from "../../../common/services/input/InputProvider";
 
 interface IPawnGLProps {
@@ -9,30 +24,37 @@ interface IPawnGLProps {
   lockIdle?: boolean;
 }
 
-const MOVE_SPEED = 0.1;
+const MOVE_SPEED = 5;
 
 // Stand in for player.
 export const PawnGL: FunctionComponent<IPawnGLProps> = ({
   position,
   lockIdle = true,
 }) => {
-  const mesh = useRef<RigidBodyApi>(null);
-  const { forward, backward, left, right } = useContext(InputContext).inputs;
-  const collisions = {
-    collU: false,
-    collD: false,
-    collL: false,
-    collR: false,
-  };
+  const rbRef = useRef<RigidBodyApi>(null);
+  const mesh = useRef<Group>(null);
+
+  const { camera } = useThree();
+
+  const ctx = useContext(InputContext);
+  const { forward, backward, left, right, rotation } = ctx.inputs;
+
+  const { nodes, materials } = useGLTF("/meshes/person-male.glb");
+  const currentNodes = useMemo(() => Object.values(nodes), [nodes]);
 
   const handleIdle = () => {};
 
   useEffect(() => {
-    const api = mesh.current as RigidBodyApi;
+    if (!mesh.current) return;
+    const api = rbRef.current as RigidBodyApi;
     api.setEnabledRotations(false, false, false);
     api.setAngularDamping(1);
     api.setLinearDamping(1);
-  }, [mesh]);
+
+    mesh.current.add(camera);
+    camera.position.set(0, 1.6, 4);
+    camera.lookAt(0, 0.3, 0);
+  }, [mesh, rbRef]);
 
   useFrame(() => {
     if (lockIdle) {
@@ -40,7 +62,11 @@ export const PawnGL: FunctionComponent<IPawnGLProps> = ({
       return;
     }
 
-    const api = mesh.current as RigidBodyApi;
+    const api = rbRef.current as RigidBodyApi;
+    const pawn = mesh.current as Group;
+
+    const trans: [number, number, number] = api.translation().toArray();
+    pawn.position.set(trans[0], 0, trans[2]);
 
     // Calculating front/side movement ...
     let frontVector = new Vector3(0, 0, 0);
@@ -48,35 +74,38 @@ export const PawnGL: FunctionComponent<IPawnGLProps> = ({
     let direction = new Vector3(0, 0, 0);
 
     // TODO: Allow player model to rotate and walk in facing direction.
+    pawn.rotation.set(0, rotation.y, 0);
 
-    frontVector.set(
-      0,
-      0,
-      Number(forward && !collisions.collU) -
-        Number(backward && !collisions.collD)
-    );
-    sideVector.set(
-      Number(right && !collisions.collR) - Number(left && !collisions.collL),
-      0,
-      0
-    );
+    frontVector.set(0, 0, backward - forward);
+    sideVector.set(left - right, 0, 0);
     direction
       .subVectors(frontVector, sideVector)
       .normalize()
-      .multiplyScalar(MOVE_SPEED);
+      .multiplyScalar(MOVE_SPEED)
+      .applyEuler(pawn.rotation);
 
-    api.applyImpulse({ x: direction.x, y: 0, z: direction.z });
+    api.setLinvel({ x: direction.x, y: 0, z: direction.z });
   });
 
   return (
     <>
-      <RigidBody colliders="hull" position={position} ref={mesh}>
-        <MeshCollider type="hull">
-          <mesh scale={1}>
-            <boxGeometry args={[1, 1, 1]} />
-          </mesh>
-        </MeshCollider>
-      </RigidBody>
+      <group>
+        <group ref={mesh} position={[0, 0, 0]}>
+          {currentNodes &&
+            currentNodes.map((node, idx) => (
+              <mesh key={idx} scale={1} geometry={(node as any).geometry} />
+            ))}
+        </group>
+        <RigidBody
+          colliders={"hull"}
+          position={position}
+          ref={rbRef}
+          type="dynamic"
+          mass={1}
+        >
+          <CapsuleCollider args={[0.3, 0.5]} />
+        </RigidBody>
+      </group>
     </>
   );
 };
